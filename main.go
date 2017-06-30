641477660659bbda277b8533097dd475d6bdf80d
chendan <dan.chen@mtime.com>
2017-06-30 15:46:03 +0800

    add shadow dial timeout

--- main.go
+++ main.go
@@ -27,14 +27,16 @@ func main() {
 
 func loadConfig() *Config {
 	var (
-		cfg     = new(Config)
-		err     error
-		config  string
-		address string
-		proxy   string
-		timeout int64
-		verbose bool
-		version bool
+		cfg               = new(Config)
+		err               error
+		config            string
+		address           string
+		proxy             string
+		timeout           int64
+		connectionTimeout int64
+		deadlineTimeout   int64
+		verbose           bool
+		version           bool
 	)
 
 	fs := NewFlags(`shadow is a simple and fast http/https proxy written by Go.
@@ -46,6 +48,8 @@ func loadConfig() *Config {
 	fs.String(&address, "addr", "a", ":1080", "Listen address")
 	fs.String(&proxy, "proxy", "p", "", "Parent proxy address")
 	fs.Int64(&timeout, "timeout", "t", 30000, "Timeout for response header of milliseconds")
+	fs.Int64(&connectionTimeout, "connection timeout", "ct", 5000, "Timeout for Dial connection of milliseconds")
+	fs.Int64(&deadlineTimeout, "deadline timeout", "dt", 10000, "Timeout for recevie after send of milliseconds")
 	fs.Bool(&verbose, "verbose", "", false, "Verbose output")
 	fs.Parse()
 
@@ -61,10 +65,12 @@ func loadConfig() *Config {
 		}
 	} else {
 		cfg = &Config{
-			Address: address,
-			Proxy:   proxy,
-			Timeout: timeout,
-			Verbose: verbose,
+			Address:           address,
+			Proxy:             proxy,
+			Timeout:           timeout,
+			Verbose:           verbose,
+			ConnectionTimeout: connectionTimeout,
+			DeadlineTimeout:   deadlineTimeout,
 		}
 	}
 	return cfg
